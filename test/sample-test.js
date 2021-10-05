70fff98d594b14ac8672e454096987f44b77dba7
eremitik <crosby.alan@gmail.com>
2021-10-05 10:53:14 +0900

    tests: cleaned up console printed items object during test

--- test/sample-test.js
+++ test/sample-test.js
@@ -28,7 +28,18 @@ describe("NFTMarket", function () {
 
     await market.connect(buyerAddress).createMarketSales(nftContractAddress, 1, { value: auctionPrice })
 
-    const items = await market.fetchMarketItems()
+    let items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
 
     console.log('items: ', items)
   })
